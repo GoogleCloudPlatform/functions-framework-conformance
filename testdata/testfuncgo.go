6883cc90fca26414ae29e7e27ac1075a6e44ef62
Julie Hockett <juliehockett@google.com>
2020-08-05 13:31:50 -0700

    test failing run

--- testdata/testfuncgo.go
+++ testdata/testfuncgo.go
@@ -10,12 +10,12 @@ import (
 
 // HTTP is a simple HTTP function that writes the request body to the response body.
 func HTTP(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
+	_, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	if err := ioutil.WriteFile("function_output.json", body, 0644); err != nil {
+	if err := ioutil.WriteFile("function_output.json", []byte("fail"), 0644); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
