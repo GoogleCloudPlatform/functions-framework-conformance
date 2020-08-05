39d531c9a2961e894fddfd65c6bceb0c8448f989
Julie Hockett <juliehockett@google.com>
2020-08-05 13:50:24 -0700

    more printf

--- action/index.js
+++ action/index.js
@@ -3,6 +3,53 @@ const github = require('@actions/github');
 const child_process = require('child_process');
 const fs = require('fs');
 
+/**
+ * Run a specified command.
+ * @param {string} cmd - command to run
+ */
+function run(cmd) {
+  child_process.exec(cmd, (error, stdout, stderr) => {
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
+    if (error) {
+      console.log(`error: ${error}`);
+      if (fs.existsSync('serverlog_stdout.txt')) {
+        fs.readFileSync('serverlog_stdout.txt', 'utf8', (err, data) => {
+          if (err) {
+            throw err; // print and move on
+          }
+          console.log(`server stdout: ${data}`);
+        });
+      } else {
+        console.log("server stdout doesn't exist");
+      }
+      if (fs.existsSync('serverlog_stderr.txt')) {
+        fs.readFileSync('serverlog_stderr.txt', 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`server stderr: ${data}`);
+        });
+      } else {
+        console.log("server stderr doesn't exist");
+      }
+      if (fs.existsSync('function_output.json')) {
+        fs.readFileSync('function_output.json', 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`function output: ${data}`);
+        });
+      } else {
+        console.log("function stdout doesn't exist");
+      }
+      return error;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
 try {
   const cmd = core.getInput('cmd');
   const functionType = core.getInput('functionType');
@@ -25,45 +72,4 @@ try {
 
 } catch (error) {
   core.setFailed(error.message);
-}
-
-/**
- * Run a specified command.
- * @param {string} cmd - command to run
- */
-function run(cmd) {
-  child_process.exec(cmd, (error, stdout, stderr) => {
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-    }
-    if (error) {
-      console.log(`error: ${error}`);
-      if (fs.existsSync('serverlog_stdout.txt')) {
-        fs.readFileSync('serverlog_stdout.txt', 'utf8', (err, data) => {
-          if (err) {
-            throw err; // print and move on
-          }
-          console.log(`server stdout: ${data}`);
-        });
-      }
-      if (fs.existsSync('serverlog_stderr.txt')) {
-        fs.readFileSync('serverlog_stderr.txt', 'utf8', (err, data) => {
-          if (err) {
-            throw err;
-          }
-          console.log(`server stderr: ${data}`);
-        });
-      }
-      if (fs.existsSync('function_output.json')) {
-        fs.readFileSync('function_output.json', 'utf8', (err, data) => {
-          if (err) {
-            throw err;
-          }
-          console.log(`function output: ${data}`);
-        });
-      }
-      return error;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
 }
