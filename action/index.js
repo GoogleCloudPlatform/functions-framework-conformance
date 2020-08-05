ea69030be1c7a53b0d948dd3bed3d624ad7d891b
Julie Hockett <juliehockett@google.com>
2020-08-05 13:11:10 -0700

    check for existence of files

--- action/index.js
+++ action/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const child_process = require("child_process");
+const child_process = require('child_process');
 const fs = require('fs');
 
 try {
@@ -11,10 +11,12 @@ try {
   // const runInContainer = core.getInput('runInContainer');
 
   // Install conformance client binary.
-  run("go install github.com/GoogleCloudPlatform/functions-framework-conformance/client");
+  run('go install github.com/GoogleCloudPlatform/functions-framework-conformance/client');
 
   // Run the client with the specified parameters.
-  run("go run github.com/GoogleCloudPlatform/functions-framework-conformance/client --cmd='" + cmd + "' --type=" + functionType + " --validate-mapping=" + validateMapping);
+  run('go run github.com/GoogleCloudPlatform/functions-framework-conformance/client --cmd=\'' +
+      cmd + '\' --type=' + functionType +
+      ' --validate-mapping=' + validateMapping);
 
 } catch (error) {
   core.setFailed(error.message);
@@ -25,34 +27,37 @@ try {
  * @param {string} cmd - command to run
  */
 function run(cmd) {
-    child_process.exec(cmd, (error, stdout, stderr) => {
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
+  child_process.exec(cmd, (error, stdout, stderr) => {
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
+    if (error) {
+      if (fs.existsSync('serverlog_stdout.txt')) {
+        fs.readFile('serverlog_stdout.txt', 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`server stdout: ${data}`);
+        });
       }
-      if (error) {
-          fs.readFile('serverlog_stdout.txt', 'utf8', (err, data) => {
-            if (err) {
-              throw err;
-            }
-            console.log(`server stdout: ${data}`);
-          });
-
-          fs.readFile('serverlog_stderr.txt', 'utf8', (err, data) => {
-            if (err) {
-              throw err;
-            }
-            console.log(`server stderr: ${data}`);
-          });
-
-          fs.readFile('function_output.txt', 'utf8', (err, data) => {
-            if (err) {
-              throw err;
-            }
-            console.log(`function output: ${data}`);
-          });
-
-          // throw error;
+      if (fs.existsSync('serverlog_stderr.txt')) {
+        fs.readFile('serverlog_stderr.txt', 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`server stderr: ${data}`);
+        });
       }
-      console.log(`stdout: ${stdout}`);
+      if (fs.existsSync('function_output.txt')) {
+        fs.readFile('function_output.txt', 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`function output: ${data}`);
+        });
+      }
+      // throw error;
+    }
+    console.log(`stdout: ${stdout}`);
   });
 }
