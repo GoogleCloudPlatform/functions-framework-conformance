7792e235f01225f3fec2425fb0d6e3ca92df0f59
Julie Hockett <juliehockett@google.com>
2020-10-05 14:11:37 -0700

    fix typo

--- client/buildpacks.go
+++ client/buildpacks.go
@@ -72,7 +72,7 @@ func (b *buildpacksFunctionServer) OutputFile() ([]byte, error) {
 }
 
 func (b *buildpacksFunctionServer) build(ctx context.Context) error {
-	builder := fmt.Sprintf("ggcr.io/fn-img/buildpacks/%s/builder:%s", b.runtime, b.tag)
+	builder := fmt.Sprintf("us.gcr.io/fn-img/buildpacks/%s/builder:%s", b.runtime, b.tag)
 	packClient, err := pack.NewClient()
 	if err != nil {
 		return fmt.Errorf("getting pack client: %v", err)
